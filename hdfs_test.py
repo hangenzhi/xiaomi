7db82161ce18f4f4c19b6dd2513699fa2d9d2f13
hgz <hgz@genzhihan.top>
2023-05-04 13:20:41 +0800

    修改1

--- hdfs_test.py
+++ hdfs_test.py
@@ -1,5 +1,8 @@
 from hdfs.client import Client
+
 # 创建连接
 client = Client("http://192.168.10.32:9870/", root="/", timeout=900, session=None)
 # 创建目录，赋予权限
-client.makedirs('/test/01', permission=775)
+# client.makedirs('/test/01', permission=775)
+a = client.list("/")
+print(a)
